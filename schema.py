d81bf48994ff709ae807c586d6a9e58e659423a6
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2023-05-01 18:52:40 +0200

    add schema genrator script

---
+++ schema.py
@@ -0,0 +1,42 @@
+from diagrams import Diagram, Cluster, Edge
+from diagrams.k8s.infra import Master, Node
+from diagrams.generic.network import VPN
+from diagrams.saas.cdn import Cloudflare
+from diagrams.oci.network import SecurityLists
+from diagrams.generic.device import Tablet
+
+graph_attr = {
+    "layout":"dot",
+    "splines":"curved",
+    "bgcolor": "transparent",
+    "beautify": "true",
+    "center": "true",
+}
+
+
+with Diagram("", show=True, direction="LR", graph_attr=graph_attr,filename="pulumi_k8s_oracle"):
+    with Cluster("Tenancy A"):
+        with Cluster("k8sMasterCompartment"):
+            with Cluster("Instance - 10.0.10.10"):
+                master = Master("k8smaster")  
+                master_wg = VPN("wireguard")
+            master_sl = SecurityLists("SecurityList\n22/tcp;51000/udp")
+    with Cluster("Tenancy B"):
+        with Cluster("k8sWorkerCompartment"):
+            with Cluster("Instance - 10.0.10.11"):
+                worker = Node("k8sworker")
+                worker_wg =  VPN("wireguard")
+            worker_sl = SecurityLists("SecurityList\n22/tcp;51000/udp")
+    cf = Cloudflare("k8s.thedodo.xyz")
+    user = Tablet("user")
+    user_wg = VPN("wireguard")
+    master >> Edge(label="10.0.100.1/32") << master_wg
+    master_wg >> Edge(label="wg0") << worker_wg
+    worker >> Edge(label="10.0.100.2/32") << worker_wg
+    cf - master_sl >> master
+    worker_sl >> worker
+    user - master_sl
+    user - worker_sl
+    user - user_wg >> Edge(label="wg0") << master_wg
+    # cf - worker_sl >> worker_fw_in >> worker
+
